356123ea3f18c80386f51a5e0c3effbec9a6f293
Jiying Cao <jiyingcao@yahoo.com>
2019-10-22 17:58:46 +0800

    Add my-app Maven project (Maven练手项目)

---
+++ my-app/README.md
@@ -0,0 +1,2 @@
+# my-app
+练习Maven使用
---
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+	private static Logger logger = Logger.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		
+		// 记录debug级别的信息  
+        logger.debug("This is debug message.");  
+        // 记录info级别的信息  
+        logger.info("This is info message.");  
+        // 记录error级别的信息  
+        logger.error("This is error message.");
+    }
+}
